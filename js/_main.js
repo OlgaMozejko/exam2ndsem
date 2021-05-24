6726011aae81e2881ed29f263ffeda31f53b71d2
keniaordonez <73820105+keniaordonez@users.noreply.github.com>
2021-05-24 14:54:19 +0200

    Nav
    kenia

--- js/_main.js
+++ js/_main.js
@@ -43,3 +43,17 @@
 	}
 		
 } )( jQuery );
+
+//Here is our function for the off-canvas menu
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "white";
+}
